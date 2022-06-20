f002a3d175a72f02fbfbdb303378fb91fa02a598
sabrinamaral <sabrinamaral@hotmail.com>
2022-06-20 20:40:53 -0300

    refact | app

--- random_user/app.js
+++ random_user/app.js
@@ -1,19 +1,13 @@
 import getElement from "./utils/getElement.js";
+import getUser from "./utils/getUser.js";
+import displayUser from "./utils/displayUser.js";
 
-const url = "https://randomuser.me/api/";
-// https://randomuser.me/documentation
-
-const img = getElement(".user-img");
-const title = getElement(".user-title");
-const value = getElement(".user-value");
 const btn = getElement(".btn");
-const btns = [...document.querySelectorAll(".icon")];
-console.log(btns);
 
-const fetchData = async () => {
-  const resp = await fetch(url);
-  const data = await resp.json();
-  console.log(data);
+const showUser = async () => {
+  const person = await getUser();
+  displayUser(person);
 };
 
-fetchData();
+window.addEventListener("DOMContentLoaded", showUser);
+btn.addEventListener("click", showUser);
