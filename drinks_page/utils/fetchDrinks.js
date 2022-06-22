bc0e2a0963838958e6a38bad8dbc7e2f96afaee5
sabrinamaral <sabrinamaral@hotmail.com>
2022-06-21 21:36:17 -0300

    module fetchDrinks

---
+++ drinks_page/utils/fetchDrinks.js
@@ -0,0 +1,11 @@
+const fetchDrinks = async (url) => {
+  try {
+    const resp = await fetch(url);
+    const data = await resp.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default fetchDrinks;
