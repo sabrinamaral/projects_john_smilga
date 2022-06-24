b919e9494c7e16420df5fe1b39c0586a549854fa
sabrinamaral <sabrinamaral@hotmail.com>
2022-06-24 19:54:17 -0300

    fetch module

---
+++ pagination/fetchFollowers.js
@@ -0,0 +1,8 @@
+const url = "https://api.github.com/users/john-smilga/followers?per_page=100";
+
+const fetchFollowers = async () => {
+  const resp = await fetch(url);
+  const data = await resp.json();
+  return data;
+};
+export default fetchFollowers;
