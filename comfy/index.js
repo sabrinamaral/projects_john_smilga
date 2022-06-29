f6dd21d2588aeccf91eb2695758da8375a2de9e3
sabrinamaral <sabrinamaral@hotmail.com>
2022-06-29 20:42:09 -0300

    add setup | js

---
+++ comfy/index.js
@@ -0,0 +1,10 @@
+// global imports
+import "./src/pages/toggleSidebar.js";
+import "./src/cart/toggleCart.js";
+import "./src/cart/setupCart.js";
+
+// specific imports
+// import fetchProducts from "./src/fetchProducts.js";
+// import { setupStore, store } from "./src/store.js";
+// import display from "./src/displayProducts.js";
+// import { getElement } from "./src/utils.js";
