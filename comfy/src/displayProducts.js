e82b06a727062c64247986ad2d57c5e4460a5cc3
sabrinamaral <sabrinamaral@hotmail.com>
2022-07-02 17:26:31 -0300

    add display products to products page

--- comfy/src/displayProducts.js
+++ comfy/src/displayProducts.js
@@ -20,11 +20,18 @@ const display = (products, element) => {
           </div>
           <footer>
             <p class="product-name">${name}</p>
-            <h4 class="product-price">$${price}</h4>
+            <h4 class="product-price">$${formatPrice(price)}</h4>
           </footer>
         </article>`;
     })
     .join("");
+
+  element.addEventListener("click", function (e) {
+    const parent = e.target.parentElement;
+    if (parent.classList.contains("product-cart-btn")) {
+      addToCart(parent.dataset.id);
+    }
+  });
 };
 
 export default display;
--- comfy/src/pages/products.js
+++ comfy/src/pages/products.js
@@ -2,3 +2,15 @@
 import "../pages/toggleSidebar.js";
 import "../cart/toggleCart.js";
 import "../cart/setupCart.js";
+
+// filter imports
+import setupCompanies from "../filters/companies.js";
+import setupSearch from "../filters/search.js";
+import setupPrice from "../filters/price.js";
+
+// specific imports
+import display from "../displayProducts.js";
+import { store } from "../store.js";
+import { getElement } from "../utils.js";
+
+display(store, getElement(".products-container"));
