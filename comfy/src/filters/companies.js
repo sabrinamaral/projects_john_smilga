f3e4c9b4fa53824876067532b46e3049f991320e
sabrinamaral <sabrinamaral@hotmail.com>
2022-07-03 12:18:47 -0300

    add filters search | companies

--- comfy/src/filters/companies.js
+++ comfy/src/filters/companies.js
@@ -1,3 +1,30 @@
-const companies = () => {};
+import { getElement } from "../utils.js";
+import display from "../displayProducts.js";
 
-export default companies;
+const setupCompanies = (store) => {
+  let companies = ["all", ...new Set(store.map((product) => product.company))];
+
+  const companiesHTML = getElement(".companies");
+  companiesHTML.innerHTML = companies
+    .map((company) => {
+      return `<button class="company-btn">${company}</button>`;
+    })
+    .join("");
+
+  companiesHTML.addEventListener("click", function (e) {
+    const element = e.target;
+    if (element.classList.contains("company-btn")) {
+      let newStore = [];
+      if (element.textContent === "all") {
+        newStore = [...store];
+      } else {
+        newStore = store.filter(
+          (product) => product.company === element.textContent
+        );
+      }
+      display(newStore, getElement(".products-container"));
+    }
+  });
+};
+
+export default setupCompanies;
